e76479664910cb9f841a162d9b5ff28669d48a0c
정두콩 <junggg097@daum.net>
2024-02-20 14:42:20 +0900

    RestClient로 TossHttpService 객체 구현

---
+++ src/main/java/com/example/toss/config/RestClientConfig.java
@@ -0,0 +1,34 @@
+package com.example.toss.config;
+
+import com.example.toss.service.TossHttpService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.support.RestClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+import java.util.Base64;
+
+@Configuration
+public class RestClientConfig {
+    @Value("${toss.secret}")
+    private String tossSecret;
+
+    @Bean
+    public RestClient tossClient() {
+        String bassicAuth = Base64.getEncoder().encodeToString((tossSecret +":").getBytes());
+        return RestClient
+                .builder()
+                .baseUrl("https://api.tosspayments.com/v1")
+                .defaultHeader("Authorization", String.format("Basic %s", bassicAuth))
+                .build();
+    }
+
+    @Bean
+    public TossHttpService httpService() {
+        return HttpServiceProxyFactory.builderFor(RestClientAdapter.create(tossClient()))
+                .build()
+                .createClient(TossHttpService.class);
+    }
+}
