a48d1ce088d83fa45eebee664ae072eaf55d60a4
정두콩 <junggg097@daum.net>
2024-02-20 14:50:22 +0900

    결제 승인까지 완료

--- src/main/java/com/example/toss/TossController.java
+++ src/main/java/com/example/toss/TossController.java
@@ -1,6 +1,8 @@
 package com.example.toss;
 
 import com.example.toss.dto.PaymentConfirmDto;
+import com.example.toss.service.OrderService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,13 +14,15 @@ import org.springframework.web.server.ResponseStatusException;
 @Slf4j
 @RestController
 @RequestMapping("/toss")
+@RequiredArgsConstructor
 public class TossController {
+    private final OrderService service;
     @PostMapping("/confirm-payment")
     public Object confirmPayment(
             @RequestBody
             PaymentConfirmDto dto
     ) {
-        log.info(dto.toString());
-        throw new ResponseStatusException(HttpStatus.NOT_IMPLEMENTED);
+        log.info("received:{}", dto.toString());
+        return service.confirmPayment(dto);
     }
 }
